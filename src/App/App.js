50c23b112e858f28c4bd1e54dafcd7f0bc316e52
Bruce Gordon <brc.gordon@gmail.com>
2020-12-16 10:46:55 -0700

    Build out controlled form for reservations

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { getData } from '../apiCalls.js';
 import List from '../List/List.js';
 import Res from '../Res/Res.js';
-
+import Form from '../Form/Form.js';
 class App extends Component {
   constructor() {
     super();
@@ -19,12 +19,18 @@ class App extends Component {
     .catch(error => this.setState({ error: error.message }))
   }
 
+  addReservation = (newRes) => {
+    this.setState({ reservations: [...this.state.reservations, newRes] })
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form
+            addReservation={ this.addReservation }
+          />
         </div>
         <div className='resy-container'>
           <List
---
+++ src/Form/Form.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      number: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const newRes = { id: Date.now(), name: this.state.name, date: this.state.date, time: this.state.time, number: parseInt(this.state.number) };
+    this.props.addReservation(newRes);
+    this.clearForm();
+  }
+
+  clearForm = () => {
+    this.setState({ name: '', date: '', time: '', number: '' })
+  }
+
+  render() {
+    return (
+      <div className='res-form'>
+        <form>
+          <label htmlFor='name'></label>
+          <input
+            type='text'
+            name='name'
+            placeholder='Name'
+            value={ this.state.name }
+            onChange={ this.handleChange }
+          ></input>
+          <label htmlFor='date'></label>
+          <input
+            type='text'
+            name='date'
+            placeholder='Date (mm/dd)'
+            value={ this.state.date }
+            onChange={ this.handleChange }
+          ></input>
+          <label htmlFor='time'></label>
+          <input
+            type='text'
+            name='time'
+            placeholder='Time'
+            value={ this.state.time }
+            onChange={ this.handleChange }
+          ></input>
+          <label htmlFor='number'></label>
+          <input
+            type='text'
+            name='number'
+            placeholder='Number of guests'
+            value={ this.state.number }
+            onChange={ this.handleChange }
+          ></input>
+          <button
+            className='make-res-button'
+            onClick={ this.handleSubmit }
+          >Make Reservation</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Form;
